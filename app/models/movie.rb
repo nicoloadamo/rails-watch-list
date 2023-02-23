d4b413edbac3e14618b103c7004377f60635edc8
Nicolo Adamo <nicolo.adamo@gmail.com>
2023-02-23 15:18:01 +0100

    corrected typo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates_uniqueness_of :title
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_115704) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_141608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_115704) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "post_url"
+    t.string "poster_url"
   end
 
   add_foreign_key "bookmarks", "lists"
